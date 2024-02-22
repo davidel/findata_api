52c24b61ed7660dccee5531df224b7eacc67b7d1
Davide Libenzi <davide.libenzi@gmail.com>
2024-02-22 07:34:40 +0100

    Added ML utilities module.

---
+++ findata_api/mlutils.py
@@ -0,0 +1,60 @@
+import numpy as np
+from py_misc_utils import alog
+from py_misc_utils import assert_checks as tas
+from py_misc_utils import date_utils as pyd
+from py_misc_utils import np_utils as pyn
+from py_misc_utils import utils as pyu
+
+
+def get_interval_indices(dim_size, size, base=None):
+  tas.check_ge(dim_size, 0)
+  tas.check_ge(size, 0)
+  tas.check_le(size, 1)
+
+  base = base if base is not None else (1.0 - size)
+  tas.check_ge(base, 0)
+  tas.check_le(base, 1)
+  tas.check_le(base + size, 1)
+
+  return int(base * dim_size), int((base + size) * dim_size)
+
+
+def get_training_indices(size, times=None, test_start_date=None, test_end_date=None,
+                         test_size=None, test_base=None, separation=None):
+  reindex = None
+  if test_size is not None and test_base is not None:
+    base, end = get_interval_indices(size, test_size, base=test_base)
+
+    test_indices = np.arange(base, end)
+  elif test_start_date is not None and test_end_date is not None:
+    if times is None:
+      alog.xraise(RuntimeError, f'Time based test indices selected, but inputs have no time information')
+    if isinstance(test_start_date, str):
+      test_start_date = pyd.parse_date(test_start_date, tz=pyd.us_eastern_timezone())
+    if isinstance(test_end_date, str):
+      test_end_date = pyd.parse_date(test_end_date, tz=pyd.us_eastern_timezone())
+    tstart = test_start_date.timestamp()
+    tend = test_end_date.timestamp()
+
+    reindex = np.argsort(times)
+    stimes = times[reindex]
+
+    test_indices = np.flatnonzero((stimes >= tstart) & (stimes < tend))
+  else:
+    alog.xraise(RuntimeError, f'Missing arguments to compute training indices')
+
+  train_indices = pyn.complement_indices(test_indices, size)
+
+  if separation is not None:
+    tmin = np.amin(test_indices) - separation
+    tmax = np.amax(test_indices) + separation
+    train_indices = train_indices[(train_indices < tmin) | (train_indices > tmax)]
+
+  if reindex is not None:
+    train_indices = np.sort(reindex[train_indices])
+    test_indices = np.sort(reindex[test_indices])
+
+  alog.debug1(f'TrainSize = {len(train_indices)}\tTestSize = {len(test_indices)}')
+
+  return pyu.make_object(train=train_indices, test=test_indices, size=size)
+
